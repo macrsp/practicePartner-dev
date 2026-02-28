8ba1def8b1b4a2e829e253988e07cd0fa1b215b9
macrsp <141585346+macrsp@users.noreply.github.com>
2026-02-28 12:06:44 -0600

    Update DB_VERSION and add SETTINGS_KEYS constants
    Updated DB_VERSION to 3 and added SETTINGS_KEYS constants.

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,17 @@
 export const DB_NAME = "suzukiDB_v4";
-export const DB_VERSION = 2;
+export const DB_VERSION = 3;
 
 export const STORES = {
   PROFILES: "profiles",
   SECTIONS: "sections",
   PLAYS: "plays",
+  SETTINGS: "settings",
+};
+
+export const SETTINGS_KEYS = {
+  MUSIC_FOLDER_HANDLE: "musicFolderHandle",
+  MUSIC_FOLDER_NAME: "musicFolderName",
+  LAST_TRACK_NAME: "lastTrackName",
 };
 
 export const DEFAULT_PROFILE_NAME = "Default";
